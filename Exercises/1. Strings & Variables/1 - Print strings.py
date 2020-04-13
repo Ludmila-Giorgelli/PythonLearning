e01d91432f3560d3a42f2ca5c026f51b24daf3f0
Ludmila-Giorgelli <ludmila.julieta.giorgelli@gmail.com>
2020-04-12 21:49:42 -0300

    Added solutions to exercises

--- Exercises/1. Strings & Variables/1 - Print strings.py
+++ Exercises/1. Strings & Variables/1 - Print strings.py
@@ -1,5 +1,21 @@
 # Code for (A) goes here
-
+print("Ludmila")
 # Code for (B) goes here
-
+name = "Ludmila"
+print(name)
 # Code for (C) goes here.
+#
+# shepherd = "Mary"
+# day = "Sunday"
+# string_in_string = "Shepherd {} is on duty on day {}.".format(shepherd, day)
+# print(string_in_string)
+
+# shepherd = "Mary"
+# day = "Sunday"
+# stuff_in_string = f"shepherd {shepherd} is on duty on day {day}"
+# print(stuff_in_string)
+
+# shepherd = "Mary"
+# day = "Sunday"
+# stuff_in_string = "Shepherd %s is on duty on day %s" % (shepherd, day)
+# print(stuff_in_string)
