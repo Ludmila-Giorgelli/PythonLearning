c0457bb37ba350190c76e7fec18a0359bd58f972
Ludmila-Giorgelli <ludmila.julieta.giorgelli@gmail.com>
2020-04-15 15:30:15 -0300

    2 - Variable update. ANSWERS

---
+++ Exercises/1. Strings & Variables/2 - Variable update.py
@@ -0,0 +1,11 @@
+#A: a=6 --> a + a + a = 6 + 6 + 6
+#A: When you Run the code it doesn't print the result
+
+#B: the 4th option, because it has the number 2 at the begining and that doesn't affect the variable
+
+#C: variable c doesn't exist
+
+#D:
+a = 1
+b = 2
+print(a + b)
