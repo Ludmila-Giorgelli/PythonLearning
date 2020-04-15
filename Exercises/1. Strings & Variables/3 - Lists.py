f9c121e98a0396daf5d46d5a803a74752686f305
Ludmila-Giorgelli <ludmila.julieta.giorgelli@gmail.com>
2020-04-15 15:46:06 -0300

    3 - Lists. ANSWERS

---
+++ Exercises/1. Strings & Variables/3 - Lists.py
@@ -0,0 +1,20 @@
+
+#1:
+animals = ["Dog", "Cow", "Beaver"]
+print(animals[1])
+
+#2:
+animals = ["Dog", "Cow", "Beaver"]
+animals.append("Cat")
+print(animals[3])
+
+#3:
+animals = ["Dog", "Cow", "Beaver"]
+print(animals[1:])
+
+animes = ["JoJo", "Hunter x Hunter", "Yuri on Ice", "Given", "One Piece"]
+print(animes[1:])
+
+#4:
+animes = ["JoJo", "Hunter x Hunter", "Yuri on Ice", "Given", "One Piece"]
+print(len(animes))
