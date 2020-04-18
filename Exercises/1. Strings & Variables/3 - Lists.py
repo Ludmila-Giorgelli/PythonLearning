3a3e144e5977a061db3ffdc1dde86908134d2c95
Ludmila-Giorgelli <ludmila.julieta.giorgelli@gmail.com>
2020-04-18 03:51:45 -0300

    3 - Lists.py

--- Exercises/1. Strings & Variables/3 - Lists.py
+++ Exercises/1. Strings & Variables/3 - Lists.py
@@ -10,10 +10,10 @@ print(animals[3])
 
 #3:
 animals = ["Dog", "Cow", "Beaver"]
-print(animals[1:])
+print(animals[-2])
 
 animes = ["JoJo", "Hunter x Hunter", "Yuri on Ice", "Given", "One Piece"]
-print(animes[1:])
+print(animes[-2])
 
 #4:
 animes = ["JoJo", "Hunter x Hunter", "Yuri on Ice", "Given", "One Piece"]
