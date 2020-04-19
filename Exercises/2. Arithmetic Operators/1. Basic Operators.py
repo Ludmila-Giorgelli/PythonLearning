1791330836e71427f1e57855ab35c8d306fabeb7
Ludmila-Giorgelli <ludmila.julieta.giorgelli@gmail.com>
2020-04-18 23:45:08 -0300

    1. Basic Operators - ANSWERS

---
+++ Exercises/2. Arithmetic Operators/1. Basic Operators.py
@@ -0,0 +1,31 @@
+
+# Code for (2)
+Num1 = int(input("Enter a number: "))
+Num2 = int(input("Enter another number: "))
+sub = Num2 - Num1
+print(sub)
+
+# Code for (3)
+Num1 = int(input("Enter a number: "))
+Num2 = int(input("Enter another number: "))
+Div = Num1/Num2
+print(Div)
+
+# Code for (4)
+Num1 = int(input("Enter a number: "))
+Num2 = int(input("Enter another number: "))
+Mult = Num1*Num2
+print(Mult)
+
+# Code for (5)
+Num1 = int(input("Enter a number: "))
+Num2 = int(input("Enter another number: "))
+if Num1 > Num2:
+    print(Num1, " Es mayor")
+else:
+    print(Num2, " Es mayor")
+
+# Code for (6)
+Num1 = int(input("Enter a number: "))
+Num2 = int(input("Enter another number: "))
+print(pow(Num1, Num2))
