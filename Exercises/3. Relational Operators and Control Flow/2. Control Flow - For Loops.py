cbfd3a4791d558b53dbc37b830b76b63c60985f7
Ludmila-Giorgelli <ludmila.julieta.giorgelli@gmail.com>
2020-05-02 16:48:24 -0300

    solved exercise 3.2.2 and 3.2.3

--- Exercises/3. Relational Operators and Control Flow/2. Control Flow - For Loops.py
+++ Exercises/3. Relational Operators and Control Flow/2. Control Flow - For Loops.py
@@ -4,11 +4,18 @@ string = "Hello"
 for x in range(number):
     print("Hello")
 
+# Code for (2)
+number = int(input("Please enter a number: "))
+i = 0
+for x in range(number):
+    i+=1
+    print(i)
+
+
 # Code for (3)
 stands = ["Star Platinum", "The World", "Crazy Diamond", "Killer Queen", "Gold Experience", "King Crimson"]
-for x in stands:
-    print(*stands, sep='\n')
-    break
+for stand in stands:
+    print(stand)
 
 # Code for (4)
 word = input("Please enter a word: ")
